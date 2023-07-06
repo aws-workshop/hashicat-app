a76a07bbced5d95bc01262fd81088f82f40a7a7f
kanitpatjim <jim2sleepy@hotmail.com>
2023-07-06 09:13:35 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -199,7 +199,6 @@ resource "aws_key_pair" "hashicat" {
 }
 
 module "s3-bucket" {
-  source              = "cloudposse/s3-bucket/aws"
-  version             = "3.1.0"
-  s3_object_ownership = "BucketOwnerEnforced"
+  source  = "app.terraform.io/jim-org/s3-bucket/aws"
+  version = "3.1.2"
 }
